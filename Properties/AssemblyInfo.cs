03ce5f3ad7cf4fd8313e61e745b8ab446d09ddc2
Alexander Frost <alex@fxfp.com>
2016-08-16 00:54:48 +0100

    Version 3.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,13 +8,13 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Futurix Ltd")]
 [assembly: AssemblyProduct("Futurix Tic Tac Toe")]
-[assembly: AssemblyCopyright("© 2011 Futurix Ltd")]
+[assembly: AssemblyCopyright("© 2011-2014 Futurix Ltd")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("d8566ff7-5d51-4c82-a65d-0d37fa605bc2")]
 
-[assembly: AssemblyVersion("2.1.*")]
+[assembly: AssemblyVersion("3.0.*")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
 
